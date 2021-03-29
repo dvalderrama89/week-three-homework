4fa3457f0c436181306b21d896e573af8e5e79b4
David Valderrama <dvalderrama08@gmail.com>
2021-03-28 21:09:14 -0700

    Added prompts and hardcoded temp password

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  let numCharacters = prompt("How many characters would you like your password to contain?");
+  let hasSpecialCharacters = confirm("Click OK to confirm including special characters.");
+  let hasNumericCharacters = confirm("Click OK to confirm including numeric characters.");
+  let hasLowerCaseCharacters = confirm("Click OK to confirm including lowercase characters.");
+  let hasUpperCaseCharacters = confirm("Click OK to confirm including uppercase characters.");
+  let password = "";
+  
+  const alphaChars = "abcdefghijklmnopqrstuvwxyz";
+  const numericChars = "0123456789";
+  const specialChars = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  password = alphaChars[0] + numericChars[4] + specialChars[2];
+  return password;
 }
 
 // Add event listener to generate button
